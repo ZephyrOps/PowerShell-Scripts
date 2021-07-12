1c4d5549571f93aa0b805201195fb7844ae1e815
ZephyrOps <74523386+ZephyrOps@users.noreply.github.com>
2021-07-11 22:42:32 -0400

    Update ADGroup-CSVExport.ps1
    Updated this command to accept user prompt for a group name and changed the path structure using the $HOME Automatic Variable and sub-expressions to standardize the export for anyone and allow them to run the script without modification.

--- ADGroup-CSVExport.ps1
+++ ADGroup-CSVExport.ps1
@@ -3,9 +3,10 @@ This script can be used for pulling the list of members from a group in Active D
 Note that this script does not recurse on pulled objects, so if there are nested sub-groups,
 then you will only see the top-level group.
 
-You will be prompted for an Identity value when ran -- you can use a dn, cn, or other valid identifier.
-
-Be sure to change the -path attribute to an existing folder on your machine.
+You will be prompted for an Group name string when ran -- you can use a dn, cn, or other valid identifier.
 #>
 
-Get-ADGroupMember | select name, samAccountName | Export-CSV -path C:\Users\Shawn\Desktop\Scripts\Export\ADGroup_Export.csv
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+$groupname = [Microsoft.VisualBasic.Interaction]::InputBox('Please enter the name of the group you would like to export','Choose a Group Name')
+
+Get-ADGroupMember -Identity $groupname | select name, samAccountName | Export-CSV -path "$HOME\Documents\$("$groupname") Export.csv" -NoTypeInformation
