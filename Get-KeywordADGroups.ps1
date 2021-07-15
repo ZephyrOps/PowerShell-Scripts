bd25c32e753bd41687580320f671d315b69d72a4
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-15 10:19:56 -0400

    Update Get-KeywordADGroups.ps1

--- Get-KeywordADGroups.ps1
+++ Get-KeywordADGroups.ps1
@@ -1,9 +1,12 @@
 <# This script can be used to find a list of all groups with a certain sub-string in their name.
 You can select which properties to use in the export by changing the properties in the Select-Object command of the scriptBlock. #>
-
-param ([String] $match = '')
-
-if ($match -eq '') { $match = Read-Host "Please enter a match string to search group names" }
-
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$True,HelpMessage='Please enter a keyword string to search for similar groups')
+    [Alias('query')]
+    [String] $match,
+    [Parameter(Mandatory=$False)]
+    [String] $LDAP
+    )
 Get-ADGroup -Filter "Name -like '*$match*'" -Properties * | 
 Select-Object name,CanonicalName,Created
