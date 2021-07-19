db194e6d37fc520965fbeb478809e8839aadb386
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-18 23:24:16 -0400

    More functionality

--- Get-SystemInformation.ps1
+++ Get-SystemInformation.ps1
@@ -1,3 +1,12 @@
-$BIOSInfo = Get-CIMInstance CIM_BIOSElement | Select-Object SerialNumber,SMBIOSBIOSVersion
-$DiskInfo = Get-WmiObject -Class Win32_logicaldisk -Filter "DriveType = '3'" |
+$BIOSInfo = Get-CIMInstance CIM_BIOSElement | Select-Object SerialNumber,Name
+#$BIOSInterface = Get-WmiObject -Namespace root\wmi -Class Lenovo_BiosSetting | Select-Object CurrentSetting
+$DiskInfo = Get-WmiObject Win32_logicaldisk -Filter "DriveType = '3'" |
             Select-Object @{L='FreeSpaceGB';E={"{0:N2}" -f ($_.FreeSpace /1GB)}},@{L="Capacity";E={"{0:N2}" -f ($_.Size/1GB)}}
+$OSVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+$hashTable = [ordered]@{ "BIOS_Serial"=$BIOSInfo.SerialNumber
+                        "BIOS_Version"=$BIOSInfo.Name
+                        "Disk_FreeSpace"=$DiskInfo.FreeSpaceGB
+                        "Disk_MaxSpace"=$DiskInfo.Capacity
+                        "OS Version"=$OSVersion
+                        "OS Build"=(Get-WMIObject Win32_OperatingSystem).BuildNumber}
+return $hashTable
