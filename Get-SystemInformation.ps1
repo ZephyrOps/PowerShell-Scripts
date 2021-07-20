417105d0a2882dcc17c1c3868aa3623b4f53d673
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-19 23:19:36 -0400

    Added more functionality

--- Get-SystemInformation.ps1
+++ Get-SystemInformation.ps1
@@ -3,7 +3,11 @@ $BIOSInfo = Get-CIMInstance CIM_BIOSElement | Select-Object SerialNumber,Name
 $DiskInfo = Get-WmiObject Win32_logicaldisk -Filter "DriveType = '3'" |
             Select-Object @{L='FreeSpaceGB';E={"{0:N2}" -f ($_.FreeSpace /1GB)}},@{L="Capacity";E={"{0:N2}" -f ($_.Size/1GB)}}
 $OSVersion = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
-$hashTable = [ordered]@{ "BIOS_Serial"=$BIOSInfo.SerialNumber
+$CVAlpha = Get-WMIObject Win32_ComputerSystemProduct | Select-object vendor, version
+$model = $CVAlpha.vendor.ToString() + " " + $CVAlpha.version.ToString()
+$hashTable = [ordered]@{"Computer Model"=$model
+                        "Computer Name"=$ENV:COMPUTERNAME
+                        "BIOS_Serial"=$BIOSInfo.SerialNumber
                         "BIOS_Version"=$BIOSInfo.Name
                         "Disk_FreeSpace"=$DiskInfo.FreeSpaceGB
                         "Disk_MaxSpace"=$DiskInfo.Capacity
