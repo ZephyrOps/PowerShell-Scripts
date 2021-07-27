e53e6644b2921767f5719b9f5a4baaac918505b6
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-26 23:54:52 -0400

    Create Enable-HubSpotAAD.ps1
    This script is designed to add a user via their employee code to two groups that enable the Office 365 HubSpot plugin in Outlook.

---
+++ Enable-HubSpotAAD.ps1
@@ -0,0 +1,5 @@
+Install-Module -name AzureAD
+Connect-AzureAD # Proceed through MFA prompt sign-in to authenticate elevated account credentials
+$hubSpotGroups = Get-AzureADGroup -searchString "HubSpot"
+$user = Get-AzureADUser -searchString "44724"
+Add-AzureADGroupMember -objectID $hubSpotGroups -refObjectID $user
