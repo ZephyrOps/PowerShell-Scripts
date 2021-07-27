c90a674ada76cf1c8891c75e44790770a599cd55
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-26 23:55:05 -0400

    Update Enable-HubSpotAAD.ps1

--- Enable-HubSpotAAD.ps1
+++ Enable-HubSpotAAD.ps1
@@ -1,5 +1,6 @@
 Install-Module -name AzureAD
 Connect-AzureAD # Proceed through MFA prompt sign-in to authenticate elevated account credentials
+
 $hubSpotGroups = Get-AzureADGroup -searchString "HubSpot"
 $user = Get-AzureADUser -searchString "44724"
 Add-AzureADGroupMember -objectID $hubSpotGroups -refObjectID $user
