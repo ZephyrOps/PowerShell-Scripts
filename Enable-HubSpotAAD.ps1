3a1f00f014c319ea11a941aa923a716f113ff802
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-27 23:26:58 -0400

    WiP

--- Enable-HubSpotAAD.ps1
+++
@@ -1,6 +0,0 @@
-Install-Module -name AzureAD
-Connect-AzureAD # Proceed through MFA prompt sign-in to authenticate elevated account credentials
-
-$hubSpotGroups = Get-AzureADGroup -searchString "HubSpot"
-$user = Get-AzureADUser -searchString "44724"
-Add-AzureADGroupMember -objectID $hubSpotGroups -refObjectID $user
