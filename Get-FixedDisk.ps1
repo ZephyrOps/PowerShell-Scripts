7f240221bc2b1c59a3f1302d5ae202739575df14
Shawn Thomas <shawn.mic.thomas@gmail.com>
2021-07-31 16:38:24 -0400

    Deleted test file

--- Get-FixedDisk.ps1
+++
@@ -1,9 +0,0 @@
-Function Get-FixedDisk {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory)]
-        [string] $Computer
-    )
-$DiskInfo = Get-WMIObject Win32_LogicalDisk -ComputerName $Computer -Filter 'DriveType=3'
-$DiskInfo
-}
